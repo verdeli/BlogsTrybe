df8aa292cefc1c481d59b21fc447222374d002b8
verdeli <jonathanverdeli@gmail.com>
2023-02-23 18:43:50 -0300

    fix: importações corretas

--- src/api/services/PostService.ts
+++ src/api/services/PostService.ts
@@ -5,7 +5,6 @@ import IServicePost from "../interfaces/IServicePost";
 
 export default class PostService implements IServicePost {
   protected model: ModelStatic<Post> = Post;   // modelStatic(type do sequelize) é a represetação das tabelas do db. Aqui esta inicializando a model.
-
   async create(dto: IPost): Promise<Post> {
     return await this.model.create({ ...dto });
   }
@@ -13,5 +12,19 @@ export default class PostService implements IServicePost {
   async readAll(): Promise<Post[]> {
     return await this.model.findAll();
   }
+
+  findById(id: number): Promise<Post | null> {
+    throw new Error("Method not implemented.");
+  }
+
+  updateById(id: number): Promise<Post> {
+    throw new Error("Method not implemented.");
+  }
+
+  deleteById(id: number): Promise<Post> {
+    throw new Error("Method not implemented.");
+  }
+
+
 
 }
